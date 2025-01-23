449e0e4703e97659f8ca33901d9bf768232edb57
Chole Seoyeong Ro <nostopzero@gmail.com>
2025-01-22 20:13:22 -0800

    Modified TodoItem/List.js

--- src/components/TodoItem.js
+++ src/components/TodoItem.js
@@ -2,25 +2,35 @@ import { Component } from "../common/Component.js";
 
 export class TodoItem extends Component {
   constructor(props) {
-    super();
-    this.props = props;
-    // this.state = { todos: [] };
-  };
+    super(props);
+    this.handleComplete = this.handleComplete.bind(this);
+    this.handleDelete = this.handleDelete.bind(this);
+  }
 
-  
+  handleComplete() {
+    this.props.todoContext.completeTodo(this.props.todo.id);
+  }
+
+  handleDelete() {
+    this.props.todoContext.deleteTodo(this.props.todo.id);
+  }
 
   render() {
-    const todoElement = document.createElement('li')
-    todoElement.className = "todo-item"
+    const todoElement = document.createElement('li');
+    todoElement.className = 'todo-item';
+    // const completedClass = this.props.todo.completed ? 'completed' : '';
+    todoElement.classList.add('todo-itme', `${this.props.todo.completed ? 'completed' : 'incomplted'}`);
+
     todoElement.innerHTML = `
-      ${this.props.todo.description}
-      <button id="todo-complete-btn">Mark Complete</button>
-      <button id="todo-delete-btn">Delete To Do</button>
+      <span class="todo-text">${this.props.todo.description}</span>
+      <div>
+        <button class="todo-complete-btn">${this.props.todo.completed ? "Mark Incompleted" : "Mark Completed"}</button>
+        <button class="todo-delete-btn">Delete</button>
+      </div>
     `;
 
-    const completeBtn = todoElement.querySelector('#todo-complete-btn').addEventListener('click', this.handleComplete);
-
-    const deleteBtn = todoElement.querySelector('#todo-delete-btn').addEventListener('click', this.handleDelete);
+    todoElement.querySelector('.todo-complete-btn').addEventListener('click', this.handleComplete);
+    todoElement.querySelector('.todo-delete-btn').addEventListener('click', this.handleDelete);
 
     return todoElement;
   }
--- src/components/TodoList.js
+++ src/components/TodoList.js
@@ -4,33 +4,54 @@ import { TodoItem } from "./TodoItem.js";
 export class TodoList extends Component {
   constructor(props) {
     super(props);
-    this.state = { todos: [] }
-    // this.updateTodoItem = this.updateTodoItem.bind(this);
-    // this.props.todoContext.subscribe(this.updateTodoItem);
-  };
+    this.state = { todos: [] };
+    this.todoListUlElement = document.createElement('ul');
+    this.updateTodoItem = this.updateTodoItem.bind(this);
+    this.props.todoContext.subscribe(this.updateTodoItem);
+  }
 
-  // updateTodoItem(todo) {
-  //   this.state.todos = todo;
-  //   console.log(this.render());
-  // }
+  updateTodoItem(todos) {
+    this.state.todos = todos;
+    this.renderTodos();
 
-  render() {
-    const todoListElement = document.createElement('div');
-    todoListElement.className = "todo-list";
+    // this.todoListUlElement.innerHTML = "";
+    // this.state.todos.forEach(todo => {
+    //   const todoItem = new TodoItem({
+    //     todo,
+    //     todoContext: this.props.todoContext
+    //   });
+    //   this.todoListUlElement.appendChild(todoItem.render());
+    // })
+  }
 
-    const todoListUlElement = document.createElement('ul');
-    todoListUlElement.className = "todo-list-ul";
-
+  renderTodos() {
+    this.todoListUlElement.innerHTML = "";
     this.state.todos.forEach(todo => {
       const todoItem = new TodoItem({
         todo,
         todoContext: this.props.todoContext
-      })
-
-      todoListUlElement.appendChild(todoItem.render());
+      });
+      this.todoListUlElement.appendChild(todoItem.render());
     })
+  }
 
-    todoListElement.appendChild(todoListUlElement);
+  render() {
+    const todoListElement = document.createElement('div');
+    todoListElement.className = 'todo-list';
+
+    this.todoListUlElement.className = "todo-list-ul";
+    this.renderTodos();
+
+    // this.todoListUlElement.innerHTML = "";
+    // this.state.todos.forEach(todo => {
+    //   const todoItem = new TodoItem({
+    //     todo,
+    //     todoContext: this.props.todoContext
+    //   });
+    //   this.todoListUlElement.appendChild(todoItem.render());
+    // })
+
+    todoListElement.appendChild(this.todoListUlElement);
 
     return todoListElement;
   }
